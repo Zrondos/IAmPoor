6faf663f54590430e5d267fca85cf3b722764424
Zrondos <zrondos@gmail.com>
2020-01-06 21:50:33 -0800

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Zachary Rondos on 1/6/20.
+//  Copyright © 2020 SuhD00DInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
